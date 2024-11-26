4709e67d74b8190262e8653a952502956362ba64
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2024-11-26 18:09:20 +0000

    se agregan los cambios

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { ContactDetails } from "./views/contactDetails.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,9 +26,9 @@ const Layout = () => {
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/contactDetails/:theid" element={<ContactDetails />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,25 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import { Card } from "../component/card.jsx";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    useEffect(()=> {
+        actions.getAgendas();
+    }, [])
+
+    return (
+        <div className="container mt-5">
+            <div className="row mb-3">
+                <div className="col-12 text-end">
+                    <Link to={"/contactDetails/1"}>
+                        <button className="btn btn-primary">Add contact</button>
+                    </Link>
+                </div>
+            </div>
+            <Card name="Name" direction="Direction" email="Email"/>
+        </div>
+    );
+};
