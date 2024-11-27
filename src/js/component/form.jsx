94d17169fab614b5c53164db9222ef216a12d2c3
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2024-11-27 16:06:37 +0000

    intento 1 en agregar el contacto

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -1,30 +1,62 @@
 import React, { useState, useEffect, useContext } from "react";
-
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
 export const Form = (props) => {
     const { store, actions } = useContext(Context);
+	const [tasks, setTasks] = useState([{label: "No tasks here, add tasks"}]);
+	
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+
+	const getName = (event) => {
+        setName(event.target.value)
+	}
+    const getPhone = (event) => {
+        setPhone(event.target.value)
+	}
+    const getEmail = (event) => {
+        setEmail(event.target.value)
+	}
+    const getAddress = (event) => {
+        setAddress(event.target.value)
+
+	}
+    const createContact = () => {
+        const contact = {
+            name: name,
+            phone: phone,
+            email: email,
+            address: address
+        };
+        console.log("newContact:", contact)
+        actions.addContact(contact)
+        // CHECAR return <Redirect to='/' />
+	}
+
 
     return (
         <form>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="floatingInput" placeholder="Full Name"/>
-                <label for="floatingInput">Full name</label>
+                <input type="text" className="form-control" id="name" placeholder="Full Name" onChange={getName}/>
+                <label for="name">Full name</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                <label for="floatingInput">Email</label>
+                <input type="email" className="form-control" id="email" placeholder="name@example.com" onChange={getEmail}/>
+                <label for="email">Email</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="number" className="form-control" id="floatingPassword" placeholder="Password"/>
-                <label for="floatingPassword">Phone</label>
+                <input type="number" className="form-control" id="phone" placeholder="Password" onChange={getPhone}/>
+                <label for="phone">Phone</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="floatingPassword" placeholder="Password"/>
-                <label for="floatingPassword">Address</label>
+                <input type="text" className="form-control" id="address" placeholder="Password" onChange={getAddress}/>
+                <label for="address">Address</label>
             </div>
-            <button type="submit" className="btn btn-primary w-100">Submit</button>
+            <button className="btn btn-primary w-100" onClick={createContact}>Submit</button>
         </form>
     );
 };
